0aff1ca3967776a5dfc5c9690a9fe2ead1e106ae
AtomAlpaca <atal@anche.no>
2025-01-09 21:41:27 +0800

    add dp/opt/wqs.md

---
+++ docs/dp/code/opt/wqs/wqs_1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+const int MAX = 5e4 + 5;
+const int MAXX = 5e5 + 5;
+
+int n, m, s, K, k, l, r, cnt, _cnt, mn = MAX, mx = -MAX; ll res, ans;
+int f[MAX];
+
+struct E { int u, v, w; } e[MAXX];
+inline bool cmp(E a, E b) { return a.w < b.w; }
+
+int find(int x) { return f[x] == x ? x : f[x] = find(f[x]); }
+
+void init()
+{
+	_cnt = cnt = res = 0;
+	std::sort(e + 1, e + m + 1, cmp);
+	for (int i = 1; i <= n; ++i) { f[i] = i; }
+}
+
+void solve()
+{
+	init();
+	for (int i = 1; i <= m and _cnt < n - 1; ++i)
+	{
+		int fu = find(e[i].u), fv = find(e[i].v);
+		if (fu == fv) { continue; }
+		++_cnt; res += e[i].w;
+		cnt += (e[i].u == s or e[i].v == s); f[fv] = fu;
+	}
+}
+
+int main()
+{
+	scanf("%d%d%d%d", &n, &m, &s, &K);
+	for (int i = 1; i <= m; ++i) { scanf("%d%d%d", &e[i].u, &e[i].v, &e[i].w); r = std::max(r, e[i].w); }
+	l = -r;
+	while (l <= r)
+	{
+		k = l + ((r - l) >> 1);
+		for (int i = 1; i <= m; ++i) { if (e[i].u == s or e[i].v == s) { e[i].w += k; } }
+		solve(); if (cnt < K) { r = k - 1; } else { ans = res - k * K; l = k + 1; }
+		mx = std::max(mx, cnt), mn = std::min(mn, cnt);
+		for (int i = 1; i <= m; ++i) { if (e[i].u == s or e[i].v == s) { e[i].w -= k; } }
+	}
+	if (K > mx or K < mn) { printf("Impossible"); return 0; }
+	printf("%lld", ans);
+}
---
+++ docs/dp/code/opt/wqs/wqs_2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+
+const int MAX = 1e5 + 5;
+
+int n, m, k, u, v, w, c, cnt, res, ans;
+int f[MAX];
+struct E { int u, v, w, c; } e[MAX];
+
+int find(int x) { if (f[x] == x) { return x; } return f[x] = find(f[x]); }
+bool cmp(E a, E b) { if (a.w == b.w) { return a.c < b.c; } return a.w < b.w; }
+
+void check(int x)
+{
+	res = cnt = 0;
+	for (int i = 1; i <= m; ++i) { if (e[i].c == 0) { e[i].w += x; } }
+	for (int i = 1; i <= n; ++i) { f[i] = i; }
+	std::sort(e + 1, e + m + 1, cmp);
+	for (int i = 1, tmp = 0; i <= m and tmp != n - 1; ++i)
+	{
+		int fu = find(e[i].u), fv = find(e[i].v);
+		if (fu == fv) { continue; }
+		f[fv] = fu; if (e[i].c == 0) { ++cnt; } res += e[i].w; ++tmp;
+	}
+	for (int i = 1; i <= m; ++i) { if (e[i].c == 0) { e[i].w -= x; } }
+}
+
+int main()
+{
+	cin >> n >> m >> k;
+	for (int i = 1; i <= m; ++i)
+	{
+		cin >> u >> v >> w >> c;
+		e[i] = {u + 1, v + 1, w, c};
+	}
+	int l = -111, r = 111, mid = 0;
+	while (l <= r)
+	{
+		mid = (l + r) >> 1;
+		check(mid);
+		if (cnt < k) { r = mid - 1; } else { l = mid + 1; ans = res - k * mid; }
+	}
+	cout << ans;
+	return 0;
+}
